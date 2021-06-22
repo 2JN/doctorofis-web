cf6e8c80ecf18d64c7bc2a5a521d658a5508dd68
Jose Navas <jesquivelnavas@gmail.com>
2021-06-22 15:14:44 -0600

    Project configuratoin

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
+
+import axios from '../lib/axios'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (localStorage.getItem('auth-token'))
+      axios.defaults.headers.common['Authorization'] =
+        localStorage.getItem('auth-token')
+  }, [])
+
   return <Component {...pageProps} />
 }
 
